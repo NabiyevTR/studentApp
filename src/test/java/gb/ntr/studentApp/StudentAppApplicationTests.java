5357e22c148695de19c04a0690bb79166b92b36e
Timur Nabiev <NabiyevTR@gmail.com>
2021-10-28 21:46:53 +0300

    Initial commit

---
+++ src/test/java/gb/ntr/studentApp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.ntr.studentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
